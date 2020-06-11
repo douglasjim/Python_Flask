88342e07febd4a6ce04f2a7648c982f83b473670
Douglasjin <30833139+douglasjim@users.noreply.github.com>
2020-06-11 07:58:15 -0300

    Up
    Up

--- jogoteca.py
+++ jogoteca.py
@@ -3,34 +3,34 @@ from flask import Flask, render_template,request,redirect,session,flash, url_for
 app = Flask(__name__)
 app.secret_key = 'alura'
 
-class Jogo:
-    def __init__(self, nome, categoria, console):
+class Videos:
+    def __init__(self, nome, tema, link):
         self.nome = nome
-        self.categoria = categoria
-        self.console = console
+        self.tema = tema
+        self.link = link
 
-listas_teste = ['texto1', 'texto2', 'texto3']
-jogo1 = Jogo('Super mario', 'Acao', 'SNES')
-jogo2 = Jogo('Megaman', 'Acao', 'Play2')
-lista = [jogo1, jogo2]
+# consigo colocar os videos apenas manualmente um video ou pelo link
+videos1 = Videos('Primeiro ', 'Eletronic','https://www.youtube.com/embed/GEmWdwxHjSs')
+videos2 = Videos('Segundo ', 'Rap','https://www.youtube.com/embed/r40pC9kyoj0')
+lista = [videos1,videos2]
 
 @app.route('/')
 def index():
-    return render_template('lista.html', titulo='Jogos', jogos=lista,lista2 = listas_teste)
+    return render_template('lista.html', titulo='Videos', videos=lista)
 
 @app.route('/form')
 def form():
     if 'usuario_logado' not in session or session['usuario_logado'] == None:
         return redirect(url_for('login', proxima=url_for('form')))
-    return render_template('form.html', titulo='Novo jogo')
+    return render_template('form.html', titulo='Novo Video')
 
 @app.route('/criar', methods=['POST',])
 def criar():
     nome = request.form['nome']
-    categoria = request.form['categoria']
-    console = request.form['console']
-    jogo = Jogo(nome,categoria,console)
-    lista.append(jogo)
+    tema = request.form['tema']
+    link = request.form['link']
+    movies = Videos(nome,tema,link)
+    lista.append(movies)
     return redirect(url_for('index'))
 
 @app.route('/login')
@@ -46,7 +46,7 @@ def autenticar():
         proxima_pagina = request.form['proxima']
         return redirect(proxima_pagina)
     else:
-        flash('Credenciais incorretas')
+        flash(request.form['usuario'] + ' Credenciais incorretas')
         return redirect(url_for('login'))
 
 @app.route('/logout')
